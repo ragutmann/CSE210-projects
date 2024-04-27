0681b14c102611363f1915d81eb5ccb1e139661b
ragutmann <rafaguzman89@gmail.com>
2024-04-27 16:27:45 -0600

    Changes to complete assignment: Learning Activity - C# Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine();
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine();
+
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        Console.WriteLine();
+
+
     }
 }
