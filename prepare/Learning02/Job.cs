be22e988c34a7e3f630dd58158dfc621ec56d5d9
ragutmann <rafaguzman89@gmail.com>
2024-05-02 18:47:24 -0600

    New changes

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -6,7 +6,7 @@ public class Job
         public string _company;
         public int _startYear;
         public int _endYear;
-
+        
         public void Display()
         {
             Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
