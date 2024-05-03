d06113f4af4930eca66a606b58733e0a5ec94385
ragutmann <rafaguzman89@gmail.com>
2024-05-02 18:42:06 -0600

    Changes

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -2,13 +2,13 @@ using System;
 
 public class Job
     {
-        public string _company;
         public string _jobTitle;
+        public string _company;
         public int _startYear;
         public int _endYear;
 
         public void Display()
         {
-            Console.WriteLine($"{_company} ({_jobTitle}) {_startYear}-{_endYear}");
+            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
         }
     }
