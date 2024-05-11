7498c280b6e7d61bd7307b3f7a421d500dbfc945
ragutmann <rafaguzman89@gmail.com>
2024-05-11 16:24:12 -0600

    changes

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,39 @@
+public class Reference
+{
+    private string _book;
+
+    private int _chapter;
+
+    private int _verse;
+
+    private int _endVerse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+    }
+
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = startVerse;
+        _endVerse = endVerse;
+    }
+
+    public string GetDisplayText()
+    {
+        // Code to get the display text of the reference
+        if (_endVerse == 0)
+        {
+            return $"{_book} {_chapter}:{_verse}";
+        }
+
+        else
+        {
+            return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
+    }
+}
