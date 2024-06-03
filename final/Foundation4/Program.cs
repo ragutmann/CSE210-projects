068b90315be6abbe9b79e71fbaaf9de0bf9ce893
ragutmann <rafaguzman89@gmail.com>
2024-06-02 21:58:18 -0600

    changes

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -8,14 +8,16 @@ class Program
         List<Activity> activities = new List<Activity>
         {
             new Running("03 Nov 2022", 30, 3.0),
-            new Running("03 Nov 2022", 30, 4.8),
-            new Cycling("03 Nov 2022", 30, 12),
+            new Running("03 Nov 2022", 90, 3.0),
+            new Cycling("03 Nov 2022", 60, 15),
             new Swimming("03 Nov 2022", 30, 10)
         };
 
+        Console.WriteLine();
         foreach (var activity in activities)
         {
             Console.WriteLine(activity.GetSummary());
         }
+        Console.WriteLine();
     }
 }
